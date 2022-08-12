c4223cdb4c23a4ebde44b27ddf406d207b6751cd
Yuusuke Miyatake <yuusuke.8686@gmail.com>
2022-08-12 11:14:32 +0900

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorECSiteSample.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorECSiteSample.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorECSiteSample.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://yusuke86.onmicrosoft.com/56a23c9e-0c46-451b-ad6b-a5d154e220a7/API.Access");
+});
+
+await builder.Build().RunAsync();
