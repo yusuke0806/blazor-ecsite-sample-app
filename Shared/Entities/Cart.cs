7938eb1c5ab703967e4854fb4ff0b46b7dc3d678
Yuusuke Miyatake <yuusuke.8686@gmail.com>
2022-11-30 23:05:08 +0900

    :construction: add Cart and CartStorage entities

---
+++ Shared/Entities/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BlazorECSiteSample.Shared.Entities;
+
+public class Cart
+{
+    public int Quantity { get; set; }
+    public Product Product { get; init; } = new();
+
+    public CartStorage ToCartStorage()
+    {
+        return new CartStorage
+        {
+            ProductId = Product.Id,
+            Quantity = Quantity
+        };
+    }
+}
+
---
+++ Shared/Entities/CartStorage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BlazorECSiteSample.Shared.Entities;
+
+public class CartStorage
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
+
